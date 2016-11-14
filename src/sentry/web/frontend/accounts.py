34b9e9aaaf3551e58fc188633f8df5995fc2bf10
Katie Lundsgaard <katie@getsentry.com>
2016-11-14 12:59:41 -0800

    Fix string formatting

--- src/sentry/web/frontend/accounts.py
+++ src/sentry/web/frontend/accounts.py
@@ -212,7 +212,7 @@ def account_settings(request):
                 user_email.set_hash()
                 user_email.save()
             user.send_confirm_email_singular(user_email)
-            msg = _('A confirmation email has been sent to %s.' % user_email.email)
+            msg = _('A confirmation email has been sent to %s.') % user_email.email
             messages.add_message(
                 request,
                 messages.SUCCESS,
@@ -425,7 +425,7 @@ def show_emails(request):
                 user_email.set_hash()
                 user_email.save()
             user.send_confirm_email_singular(user_email)
-            msg = _('A confirmation email has been sent to %s.' % user_email.email)
+            msg = _('A confirmation email has been sent to %s.') % user_email.email
             messages.add_message(
                 request,
                 messages.SUCCESS,
@@ -447,7 +447,7 @@ def show_emails(request):
                 new_email.save()
             # send confirmation emails to any non verified emails
             user.send_confirm_email_singular(new_email)
-            msg = _('A confirmation email has been sent to %s.' % new_email.email)
+            msg = _('A confirmation email has been sent to %s.') % new_email.email
             messages.add_message(
                 request,
                 messages.SUCCESS,
